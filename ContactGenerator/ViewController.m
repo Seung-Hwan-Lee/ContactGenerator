a5af84a4854a7ee25489ae0cc787b7a021ad4bd3
shwan.lee <shwan.lee@linecorp.com>
2016-01-20 10:08:57 +0900

    .

--- ContactGenerator/ViewController.m
+++ ContactGenerator/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+@import AddressBook;
 
 @interface ViewController ()
 
@@ -22,6 +23,144 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)tapGenerateButton:(id)sender {
+    if (ABAddressBookGetAuthorizationStatus() == kABAuthorizationStatusDenied ||
+        ABAddressBookGetAuthorizationStatus() == kABAuthorizationStatusRestricted){
+        //1
+        NSLog(@"Denied");
+        UIAlertView *cantAddContactAlert = [[UIAlertView alloc] initWithTitle: @"Cannot Add Contact" message: @"You must give the app permission to add the contact first." delegate:nil cancelButtonTitle: @"OK" otherButtonTitles: nil];
+        [cantAddContactAlert show];
+    } else if (ABAddressBookGetAuthorizationStatus() == kABAuthorizationStatusAuthorized){
+        //2
+        NSLog(@"Authorized");
+        [self generateSimulatorContacts];
+        [self generateDeviceContacts];
+        NSLog(@"\nCompleted!");
+    } else{ //ABAddressBookGetAuthorizationStatus() == kABAuthorizationStatusNotDetermined
+        //3
+        NSLog(@"Not determined");
+        ABAddressBookRequestAccessWithCompletion(ABAddressBookCreateWithOptions(NULL, nil), ^(bool granted, CFErrorRef error) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (!granted){
+                    //4
+                    UIAlertView *cantAddContactAlert = [[UIAlertView alloc] initWithTitle: @"Cannot Add Contact" message: @"You must give the app permission to add the contact first." delegate:nil cancelButtonTitle: @"OK" otherButtonTitles: nil];
+                    [cantAddContactAlert show];
+                    return;
+                }
+                //5
+                [self generateSimulatorContacts];
+                [self generateDeviceContacts];
+                NSLog(@"\nCompleted!");
+            });
+        });
+    }
+}
+
+- (void)generateSimulatorContacts
+{
+    //    4800 0081
+    //    0099
+    //
+    //
+    //    5000 0081
+    //    0099
+    //
+    //
+    //    5800 0081
+    //    0099
+    //
+    //
+    //    6000 0081
+    //    0099
+    //
+    //
+    //    6004 0081
+    //    0099
+    //
+    //
+    //    6800 0091
+    //    0099
+    //
+    //
+    //    6804 0081
+    //    0099
+    ABAddressBookRef addressBookRef = ABAddressBookCreateWithOptions(NULL, nil);
+    
+    NSDictionary<NSString*,NSString*> *countryPrefixDic = @{@"KR" : @"010",
+                                                            @"JP" : @"080"};
+    [countryPrefixDic enumerateKeysAndObjectsUsingBlock:^(NSString * _Nonnull countryCode, NSString * _Nonnull countryTel, BOOL * _Nonnull stop) {
+        
+        NSDictionary<NSString*,NSString*> *contactDic = @{@"4s"  : @"4800",
+                                                          @"5"   : @"5000",
+                                                          @"5s"  : @"5800",
+                                                          @"6"   : @"6000",
+                                                          @"6+"  : @"6004",
+                                                          @"6s"  : @"6800",
+                                                          @"6s+" : @"6804"};
+        [[[contactDic allKeys] sortedArrayUsingSelector:@selector(compare:)] enumerateObjectsUsingBlock:^(NSString * _Nonnull model, NSUInteger idx, BOOL * _Nonnull stop) {
+            
+            NSString * telPrefix = contactDic[model];
+            
+            for (NSInteger tel2 = 81 ; tel2 < 100 ; tel2++) {
+                
+                NSString *contactName = [NSString stringWithFormat:@"%@ sim %@ %ld.%ld", countryCode, model, (long)(tel2/10), (long)(tel2%10)];
+                NSString *telNumber = [NSString stringWithFormat:@"%@%@%04ld", countryTel, telPrefix, (long)tel2];
+                
+                ABRecordRef pet = ABPersonCreate();
+                ABRecordSetValue(pet, kABPersonLastNameProperty, (__bridge CFStringRef)contactName, nil);
+                
+                ABMutableMultiValueRef phoneNumbers = ABMultiValueCreateMutable(kABMultiStringPropertyType);
+                ABMultiValueAddValueAndLabel(phoneNumbers, (__bridge CFStringRef)telNumber, kABPersonPhoneMainLabel, NULL);
+                ABRecordSetValue(pet, kABPersonPhoneProperty, phoneNumbers, nil);
+                
+                ABAddressBookAddRecord(addressBookRef, pet, nil);
+                
+                NSLog(@"added %@ %@", contactName, telNumber);
+            }
+        }];
+    }];
+    
+    ABAddressBookSave(addressBookRef, nil);
+}
+
+- (void)generateDeviceContacts
+{
+    ABAddressBookRef addressBookRef = ABAddressBookCreateWithOptions(NULL, nil);
+    
+    NSDictionary<NSString*,NSString*> *countryPrefixDic = @{@"KR" : @"010",
+                                                            @"JP" : @"080"};
+    [countryPrefixDic enumerateKeysAndObjectsUsingBlock:^(NSString * _Nonnull countryCode, NSString * _Nonnull countryTel, BOOL * _Nonnull stop) {
+        
+        NSDictionary<NSString*,NSString*> *contactDic = @{@"iPhone 4"    : @"32715501",
+                                                          @"iPhone 4 b"  : @"32715503",
+                                                          @"iPhone 5s"   : @"32715507",
+                                                          @"iPhone 5s b" : @"32715508",
+                                                          @"iPhone 6 b"  : @"21442163",
+                                                          @"iPhone 6+"   : @"21442162",
+                                                          @"iPhone 6+ b" : @"21442168"};
+        [[[contactDic allKeys] sortedArrayUsingSelector:@selector(compare:)] enumerateObjectsUsingBlock:^(NSString * _Nonnull contactName, NSUInteger idx, BOOL * _Nonnull stop) {
+            
+            NSString * telNumber = contactDic[contactName];
+            
+            contactName = [NSString stringWithFormat:@"%@ %@", countryCode, contactName];
+            telNumber = [NSString stringWithFormat:@"%@%@", countryTel, telNumber];
+            
+            ABRecordRef pet = ABPersonCreate();
+            ABRecordSetValue(pet, kABPersonLastNameProperty, (__bridge CFStringRef)contactName, nil);
+            
+            ABMutableMultiValueRef phoneNumbers = ABMultiValueCreateMutable(kABMultiStringPropertyType);
+            ABMultiValueAddValueAndLabel(phoneNumbers, (__bridge CFStringRef)telNumber, kABPersonPhoneMainLabel, NULL);
+            ABRecordSetValue(pet, kABPersonPhoneProperty, phoneNumbers, nil);
+            
+            ABAddressBookAddRecord(addressBookRef, pet, nil);
+            
+            NSLog(@"added %@ %@", contactName, telNumber);
+        }];
+    }];
+    
+    ABAddressBookSave(addressBookRef, nil);
 }
 
 @end
